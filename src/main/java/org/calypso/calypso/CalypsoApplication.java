ddb3ca2c8372e1df3aa2a9595122e21b4c880f8a
Jojopok <zaiajonathan@gmail.com>
2024-08-21 14:48:11 +0200

    my first commit

---
+++ src/main/java/org/calypso/calypso/CalypsoApplication.java
@@ -0,0 +1,13 @@
+package org.calypso.calypso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalypsoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalypsoApplication.class, args);
+	}
+
+}
